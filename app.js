f36a47907315879514f3008f5c6dfe37d15889fb
Landon George <landon.robert.george@gmail.com>
2022-08-14 21:10:54 -0500

    refactor

--- app.js
+++ app.js
@@ -2,34 +2,59 @@
 
 const sketchContainer = document.querySelector("#sketch-container");
 const sketchBtnClear = document.querySelector("#sketch-btn-clear");
+const gridSizeLabel = document.querySelector('#grid-size-label')
+const gridSizeSlider = document.querySelector('#grid-size-slider')
+
+const changeSizeSlider = (value) => {
+  gridSizeSlider.value = +value
+}
+
+const changeSliderLabel = (value) => {
+  gridSizeLabel.textContent = `${value} x ${value} grid`
+}
+
+const resetSketchContainer = () => {
+  for (let div of sketchContainer.querySelectorAll('.sketch-item')) {
+    div.classList.remove("selected");
+  }
+}
+
+const removeSketchContainer = () => {
+  for (let div of sketchContainer.querySelectorAll('.sketch-item')) {
+    div.remove()
+  }
+}
 
 const fillSketchContainer = function (gridSize = 16) {
   for (let i = 0; i < gridSize ** 2; i++) {
-    sketchContainer.setAttribute(
-      "style",
-      `grid-template-columns: repeat(${gridSize}, 1fr);`
-    );
-    const div = document.createElement("div");
-    div.classList.add("sketch-item");
-    div.classList.add("unselected");
+    sketchContainer.setAttribute("style", `grid-template-columns: repeat(${gridSize}, 1fr);`)
+    const div = document.createElement("div")
+    div.classList.add("sketch-item")
     div.addEventListener("mouseover", function (e) {
-      this.classList.remove("unselected");
-      this.classList.add("selected");
+      this.classList.add("selected")
     });
-    sketchContainer.appendChild(div);
+    sketchContainer.appendChild(div)
   }
-};
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  let value = 50
+  changeSliderLabel(value)
+  changeSizeSlider(value)
+  fillSketchContainer(value)
+})
+
+gridSizeSlider.addEventListener('input', (e) => {
+  let value = e.target.value
+  changeSliderLabel(value)
+  removeSketchContainer()
+  fillSketchContainer(value)
+})
+
 
 sketchBtnClear.addEventListener("click", () => {
-  sketchContainer.innerHTML = "";
-  let gridSize;
-  while (true) {
-    gridSize = +prompt("Grid size?");
-    if (gridSize > 100) alert("Number cannot be greater than 100");
-    else if (gridSize < 1) alert("Number must be greater than 0");
-    else break;
-  }
-  fillSketchContainer(gridSize);
+  resetSketchContainer()
 });
 
-fillSketchContainer();
+
+// fillSketchContainer();
